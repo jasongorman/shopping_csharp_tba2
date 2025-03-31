57eec9a276c4bf290dd3de57db093138b004b0e8
Jason Gorman <jason.gorman@codemanship.com>
2025-03-31 09:26:27 +0100

    Added test for total of empty basket and moved Basket class into its own project

---
+++ Shopping.Model/Basket.cs
@@ -0,0 +1,6 @@
+﻿namespace Shopping.Model;
+
+public class Basket
+{
+    public double Total => 0.0;
+}
---
+++ Shopping.Test/BasketTotalTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using Shopping.Model;
+
+namespace Shopping.Test;
+
+public class BasketTotalTest
+{
+    [Test]
+    public void TotalOfEmptyBasket()
+    {
+        Basket basket = new Basket();
+        Assert.That(basket.Total, Is.EqualTo(0.0));
+    }
+}
